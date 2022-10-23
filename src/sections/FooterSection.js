2ca39de78db59af91da56c9f84d32d401c7f5662
AmDizzy <abdojoh@hotmail.com>
2022-10-23 18:00:20 +0200

    made the product grid button and hover effects touch screen friendly

--- src/sections/FooterSection.js
+++ src/sections/FooterSection.js
@@ -4,11 +4,11 @@ const FooterSection = () => {
   return (
     <footer>
         <div className="social-media">
-            <a href="#" target="_blank"><i className="fa-brands fa-facebook-f"></i></a>
-            <a href="#" target="_blank"><i className="fa-brands fa-instagram"></i></a>
-            <a href="#" target="_blank"><i className="fa-brands fa-twitter"></i></a>
-            <a href="#" target="_blank"><i className="fa-brands fa-google"></i></a>
-            <a href="#" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.facebook.com/" target="_blank"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-instagram"></i></a>
+            <a href="https://www.twitter.com/" target="_blank"><i className="fa-brands fa-twitter"></i></a>
+            <a href="https://www.google.com/" target="_blank"><i className="fa-brands fa-google"></i></a>
+            <a href="https://www.linkedin.com/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
         </div>
         <div className="copyright-information">
             <span>© 2022 Fixxo. All Rights Reserved</span>
